e8cf06971d216e5afc1841bf35f2c4a377dab0de
tom-maria500 <maria.tom06@gmail.com>
2020-12-31 17:23:20 -0500

    added login page

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/login-page/LoginPage'; 
 import './App.css';
-import CreateAccount from './components/CreateAccount';
-import LogInAccount from './components/login-account/LogInAccount';
-import HomePage from './components/HomePage';
 
 class App extends React.Component {
   render() {
     return(
       <Router>
         <Switch>
-          {/* <Route path='/' exact={true} component={CreateAccount} /> */}
-          <Route path='/' exact={true} component={LogInAccount} />
-          <Route path='/home' exact={true} component={HomePage} />
+          <Route path='/' exact={true} component={LoginPage} />
+          {/* <Route path='/home' exact={true} component={HomePage} /> */}
         </Switch>
       </Router>
     );
